598b03c7f1f04efc51b30714110e2fea93a013a3
А336 <dvlplf@gmail.com>
2021-10-07 17:34:25 +0300

    lesson 3. LinkedList (part 1)

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,8 +1,16 @@
 package home;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello! world");
+    }
+
+    public static void printHello() {
+        List<Integer> numbers = new ArrayList<>();
     }
 }
