c6a9be54635db049cbbb43d3ecc43778b8126534
dvlplf <dvlplf@gmail.com>
2021-10-07 23:53:21 +0300

    lesson4. crate MyLinkedList (create method add)

---
+++ src/lesson4/MyLinkedList.java
@@ -0,0 +1,59 @@
+package lesson4;
+
+import java.util.Arrays;
+
+public class MyLinkedList {
+    private Node head;
+    private int size;
+    public void add(int value) {
+        //if first add
+        if (head == null) {
+            this.head = new Node(value);
+        } else {
+            Node temp = head;
+            while(temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(new Node(value));
+        }
+        size++;
+    }
+
+    public String toString() {
+        int[] result = new int[size];
+        int index = 0;
+        Node temp = head;
+        while(temp != null) {
+            result[index] = temp.getValue();
+            index++;
+            temp = temp.getNext();
+        }
+
+        return Arrays.toString(result);
+    }
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/lesson4/Test.java
@@ -0,0 +1,15 @@
+package lesson4;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        list.add(100);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.toString());
+    }
+}
